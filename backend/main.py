90888583b1caaa7f08f98feda100025a3025f7b4
Varguez DuranJose Emilio <141971906+EmilioVD@users.noreply.github.com>
2026-08-24 19:16:29 -0600

    Clase 2 _24_08_2026

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+import os
+
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+
+app = FastAPI()
+
+mongo_uri = os.getenv("MONGO_URI", "mongodb://admin_user:web3@mongo_container:27017/")
+mongo_client = MongoClient(mongo_uri)
+database = mongo_client["Web3"]
+productos = database["productos"]
+
+@app.get("/")
+def default_route():
+    return {"message": "Uvicorn is running!"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/productos")
+def get_productos():
+    return list(productos.find({}, {"_id": 0}))
