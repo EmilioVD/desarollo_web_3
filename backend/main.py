d8fd065d6383dd22341b7851feef1304db48e9b4
Varguez DuranJose Emilio <141971906+EmilioVD@users.noreply.github.com>
2026-09-11 18:50:12 -0600

    Clase_11_09_2026

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 app = FastAPI()
 
-mongo_uri = os.getenv("MONGO_URI", "mongodb://admin_user:web3@mongo_container:27017/")
+mongo_uri = os.getenv("MONGO_URI", "mongodb://admin_user:web3@mongo-service.default.svc.cluster.local:27017/")
 mongo_client = MongoClient(mongo_uri)
 database = mongo_client["Web3"]
 productos = database["productos"]
